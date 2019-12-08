b09491593e364d4b0176d4398078973fbb050c72
Vuonglestudent <55451678+Vuonglestudent@users.noreply.github.com>
2019-12-08 09:55:21 +0700

    update

--- CacHam_vuong.sql
+++ CacHam_vuong.sql
@@ -1,6 +1,6 @@
-use QuanLyTuyenSinh;
+﻿use QuanLyTuyenSinh;
 go
----Function
+---Function--Vương
 if OBJECT_ID('ShowHOSOTHISINH') is not null drop function ShowHOSOTHISINH;
 go
 
@@ -9,37 +9,8 @@ create function ShowHOSOTHISINH()
 	return (select * from HOSOTHISINH);
  go
  -----------------------------------------------------------------------------
- --Store PROC
- if OBJECT_ID('CHECKLOGIN') is not null drop PROC CHECKLOGIN;
- go
+ --Store PROC--Vương
 
-create PROC CHECKLOGIN
- @UserName varchar(50),
- @Pass varchar(50)
-as
-begin  
-	select * from dbo.Account where UserName=@UserName AND Pass=@Pass;
-end
-go
-
-
-
-use QuanLyTuyenSinh
-go
-if OBJECT_ID('LOADAccount') is not null drop PROC LOADAccount;
-go
-
-create PROC LOADAccount
-as
-begin 
-	select ID,NamePerson, UserName, TypePerson 
-	from Account;
-end
-go
-
-
-use QuanLyTuyenSinh
-go
 if OBJECT_ID('LOADNganh') is not null drop PROC LOADNganh;
 go
 
@@ -49,9 +20,6 @@ begin
 	select MaNganh,TenNganh,ChiTieu,DiemChuan
 	from NGANH
 end
-go
-
-use QuanLyTuyenSinh
 go
 
 if OBJECT_ID('LOADDoiTuong') is not null drop PROC LOADDoiTuong;
@@ -66,21 +34,6 @@ begin
 end
 go
 
-use QuanLyTuyenSinh;
-go
-if OBJECT_ID('LOADHoKhau') is not null drop PROC LOADHoKhau;
-go
-
-create PROC LOADHoKhau
-as
-begin 
-	select MaHoKhau, TenTinh,TenHuyen,KHUVUC.MaKhuVuc,TenKhuVuc,DiemCongKV
-	from HOKHAU join KHUVUC on HOKHAU.MaKhuVuc=KHUVUC.MaKhuVuc
-end
-go
-
-use QuanLyTuyenSinh;
-go
 if OBJECT_ID('LOADKhuVuc') is not null drop proc LOADKhuVuc;
 go
 
@@ -91,9 +44,32 @@ begin
 	from KHUVUC
 end
 go
+-----------------------------------------------------------------
+--------------------Account--------------------------------------
 
-use QuanLyTuyenSinh;
+ if OBJECT_ID('CHECKLOGIN') is not null drop PROC CHECKLOGIN;
+ go
+
+create PROC CHECKLOGIN
+ @UserName varchar(50),
+ @Pass varchar(50)
+as
+begin  
+	select * from dbo.Account where UserName=@UserName AND Pass=@Pass;
+end
 go
+
+if OBJECT_ID('LOADAccount') is not null drop PROC LOADAccount;
+go
+
+create PROC LOADAccount
+as
+begin 
+	select ID,NamePerson, UserName, TypePerson 
+	from Account;
+end
+go
+
 if OBJECT_ID('INSERTAccount') is not null drop proc INSERTAccount;
 go
 
@@ -117,11 +93,6 @@ begin
 end
 go
 
-
-
-
-
-
 if OBJECT_ID('RePassAccount') is not null drop proc RePassAccount;
 go
 
@@ -134,7 +105,6 @@ begin
 end
 go
 
-
 if OBJECT_ID('DELETEAccount') is not null drop proc DELETEAccount;
 go
 
@@ -145,7 +115,18 @@ begin
 	Delete Account where ID=@Id;
 end
 go
+------------------------------------------------------
+------------------HoKhau------------------------------
+if OBJECT_ID('LOADHoKhau') is not null drop PROC LOADHoKhau;
+go
 
+create PROC LOADHoKhau
+as
+begin 
+	select MaHoKhau, TenTinh,TenHuyen,KHUVUC.MaKhuVuc,TenKhuVuc,DiemCongKV
+	from HOKHAU join KHUVUC on HOKHAU.MaKhuVuc=KHUVUC.MaKhuVuc
+end
+go
 
 if OBJECT_ID('INSERTHoKhau') is not null drop proc INSERTHoKhau
 go
@@ -164,9 +145,6 @@ begin
 		values(@tenhuyen,@tentinh,@MaKhuVuc);
 end
 go
-
-
-
 
 if OBJECT_ID('DELETEHoKhau') is not null drop proc DELETEHoKhau;
 go
@@ -201,10 +179,10 @@ begin
 		update HOKHAU set TenTinh=@tentinh, TenHuyen=@tenhuyen, MaKhuVuc=@checkmakhuvuc where MaHoKhau=@mahokhau
 end
 go
-
-
-if OBJECT_ID('LOADTableHoSoThiSinh') is not null drop PROC LOADTableHoSoThiSinh;
- go
+----------------------------------------------------------------------
+-------------------------------Hosothisinh----------------------------
+if OBJECT_ID('LOADTableHoSoThiSinh') is not null drop PROC LOADTableHoSoThiSinh
+go
 
  create PROC LOADTableHoSoThiSinh
  as
@@ -227,34 +205,535 @@ begin
 	Delete HOSOTHISINH where SBD=@sbd;
 end
 go
+
+if OBJECT_ID('INSERTHoSoThiSinh') is not null drop proc INSERTHoSoThiSinh;
+go
+
+create proc INSERTHoSoThiSinh
+@sbd int,@hodem varchar(50),@ten varchar(50),@ngaysinh smalldatetime,@gioitinh bit,@tenhuyen varchar(50),@tentinh varchar(50),@doituong varchar(50),
+@dantoc varchar(50),@toan float,@ly float,@hoa float,@sinh float,@van float,@su float,@dia float,@anhvan float
+as
+begin 
+		declare @mahokhau int
+			select @mahokhau=MaHoKhau
+			from HOKHAU
+			where TenHuyen=@tenhuyen and TenTinh=@tentinh
+		declare @madoituong int
+			select @madoituong = MaDoiTuong
+			from DOITUONG
+			where LoaiDoiTuong=@doituong
+		declare  @madantoc int
+			select @madantoc=MaDanToc
+			from DANTOC
+			where TenDanToc=@dantoc
+		if(@mahokhau is not null and @madoituong is not null and @madantoc is not null)
+		begin
+			Insert HOSOTHISINH(SBD,HoDem,Ten,NgaySinh,GioiTinh,MaHoKhau,MaDoiTuong,MaDanToc,Toan,Ly,Hoa,Sinh,Van,Su,Dia,AnhVan)
+			values (@sbd,@hodem,@ten, @ngaysinh,@gioitinh,@mahokhau,@madoituong,@madantoc,@toan,@ly,@hoa,@sinh,@van,@su,@dia,@anhvan)
+		end;
+end;
+GO
+
+if OBJECT_ID('UPDATEHoSoThiSinh') is not null drop proc UPDATEHoSoThiSinh;
+go
+
+create proc UPDATEHoSoThiSinh
+@sbd int,@hodem varchar(50),@ten varchar(50),@ngaysinh smalldatetime,@gioitinh bit,@tenhuyen varchar(50),@tentinh varchar(50),@doituong varchar(50),
+@dantoc varchar(50),@toan float,@ly float,@hoa float,@sinh float,@van float,@su float,@dia float,@anhvan float
+as
+begin 
+	declare @mahokhau int
+		select @mahokhau=MaHoKhau
+		from HOKHAU
+		where TenHuyen=@tenhuyen and TenTinh=@tentinh
+	declare @madoituong int
+		select @madoituong = MaDoiTuong
+		from DOITUONG
+		where LoaiDoiTuong=@doituong
+	declare  @madantoc int
+		select @madantoc=MaDanToc
+		from DANTOC
+		where TenDanToc=@dantoc
+		if(@mahokhau is not null and @madoituong is not null and @madantoc is not null)
+		begin
+			Update HOSOTHISINH set HoDem=@hodem , Ten=@ten , NgaySinh=@ngaysinh , GioiTinh=@gioitinh,
+			MaHoKhau=@mahokhau , MaDoiTuong=@madoituong , MaDanToc=@madantoc , Toan=@toan , Ly=@ly , 
+			Hoa=@hoa , Sinh=@sinh , Van=@van , Su=@su , Dia=@dia , AnhVan=@anhvan
+			where SBD=@sbd;
+		end;
+end
+GO
+
+
+----------------------------------------------------------------------------------------------------------------------
+------------Nguyện Vọng---------
+
+if OBJECT_ID('INSERTNguyenVong') is not null drop proc INSERTNguyenVong;
+go
+
+create proc INSERTNguyenVong
+@sbd int,@tennganh varchar(50),@khoi varchar(50),@nguyenvong int
+as
+	declare @manganh int
+	select @manganh=MaNganh
+	from NGANH
+	where TenNganh=@tennganh
+	Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong)
+	values(@sbd,@manganh,@khoi,@nguyenvong)
+go
+
+if OBJECT_ID('TriggerInsertNguyenVong') is not null drop trigger TriggerInsertNguyenVong;
+go
+
+create trigger TriggerInsertNguyenVong
+on NGUYENVONG
+instead of Insert
+as
+declare @sbd int,@manganh varchar(50),@khoi varchar(10),@nguyenvong int,
+		@diemTB float,@danhgia varchar(50),@TestRowCount int;
+select	@TestRowCount=COUNT(*) from inserted;
+if @TestRowCount=1
+	begin
+		select @sbd=SBD,@manganh=MaNganh,@khoi=Khoi,@nguyenvong=NguyenVong
+		from inserted;
+		if(@sbd is not null and @manganh is not null and @khoi is not null and @nguyenvong is not null)
+		begin
+			if(@khoi=N'A')
+			begin
+				select @diemTB=Toan+Ly+Hoa
+				from HOSOTHISINH
+				where SBD=@sbd;
+				if(@diemTB>=0)
+				begin
+					--print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+
+					--select @danhgia;
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+				end;
+			end;
+		else
+			if(@khoi=N'A1')
+			begin
+				select @diemTB=Toan+Ly+AnhVan
+				from HOSOTHISINH
+				where SBD=@sbd;
+
+				if(@diemTB>=0)
+				begin
+					--print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+
+					--select @danhgia;
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+				end;
+			end;
+			else
+			if(@khoi=N'B')
+			begin
+				select @diemTB=Toan+Hoa+Sinh
+				from HOSOTHISINH
+				where SBD=@sbd;
+
+				if(@diemTB>=0)
+				begin
+					--print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+
+					--select @danhgia;
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+				end;
+			end;
+			else
+			if(@khoi=N'C')
+			begin
+				select @diemTB=Van+Su+Dia
+				from HOSOTHISINH
+				where SBD=@sbd;
+
+				if(@diemTB>=0)
+				begin
+					--print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+
+					--select @danhgia;
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+				end;
+			end;
+			else
+			if(@khoi=N'D')
+			begin
+				select @diemTB=Toan+Van+AnhVan
+				from HOSOTHISINH
+				where SBD=@sbd;
+
+				if(@diemTB>=0)
+				begin
+					--print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+
+					--select @danhgia;
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+				end;
+			end;
+			else
+			throw 5000,'Lỗi khối',1;
+		end;
+		else
+		throw 5000,'Lỗi không có dữ liệu từ 4 biến',1;
+	end;
+else
+	throw 5000,'Bạn bị lỗi insert Nguyện Vọng',1;
+go
+
+if OBJECT_ID('DELETENguyenVong') is not null drop proc DELETENguyenVong
+go
+
+create proc DELETENguyenVong
+@sbd int,
+@tennganh varchar(50),
+@khoi varchar(50),
+@nguyenvong int
+as
+begin
+	declare @manganh int
+	select @manganh=MaNganh
+	from NGANH
+	where TenNganh=@tennganh;
+
+	Delete NGUYENVONG where SBD=@sbd and MaNganh=@manganh and Khoi=@khoi and NguyenVong=@nguyenvong;
+end;
+go
+
+
+-----------------------------------------------------------------------
+-------------------------Diễm------------------------------------------
+
+if OBJECT_ID('INSERTNganh') is not null drop proc INSERTNganh;
+go
+
+create proc INSERTNganh
+@MaNganh varchar(50), @TenNganh varchar(50), @ChiTieu int, @DiemChuan float
+
+as
+begin 
+	declare @CheckMaNganh varchar(50);
+	
+	select @CheckMaNganh=@MaNganh
+	from NGANH
+	where MaNganh=@MaNganh;
+
+	Insert NGANH (MaNganh, TenNganh, ChiTieu, DiemChuan)
+		values(@MaNganh, @TenNganh, @ChiTieu, @DiemChuan);
+end
+go
+
+
+if OBJECT_ID('DELETENganh') is not null drop proc DELETENganh;
+go
+
+create proc DELETENganh
+@id int
+as
+begin 
+	Delete NGANH where MaNganh=@id;
+end
+go
+
+if OBJECT_ID('EDITNganh') is not null drop proc EDITNganh;
+go
+
+create proc EDITNganh
+@MaNganh varchar (50),
+@TenNganh varchar(100),
+@ChiTieu int,
+@DiemChuan float
+as
+begin 
+	declare @checkmanganh varchar(50)
+	select @checkmanganh=MANGANH 
+	from NGANH
+	where @MaNganh=MaNganh;
+
+	if @checkmanganh = @MaNganh 
+		update NGANH set TenNganh=@TenNganh, ChiTieu=@ChiTieu where MANGANH=@MaNganh 
+	else
+		update NGANH set MaNganh=@checkmanganh, TenNganh=@TenNganh, ChiTieu=@ChiTieu, DiemChuan=@DiemChuan where MANGANH=@MaNganh 
+end
+go
+
+---//////////////////////////////////
+
+if OBJECT_ID('INSERTDoituong') is not null drop proc INSERTDoituong
+go
+
+create proc INSERTDoituong
+@LoaiDoiTuong varchar(100), @DiemCongDTuong float
+as
+begin 
+	declare @MaDoiTuong int;
+	
+	select @MaDoiTuong=@MaDoiTuong
+	from DOITUONG
+	where LoaiDoiTuong=@LoaiDoiTuong;
+
+	Insert DOITUONG(MaDoituong, LoaiDoiTuong, DiemCongDTuong)
+		values(@MaDoiTuong, @LoaiDoiTuong, @DiemCongDTuong);
+end
+go
+
+if OBJECT_ID('DELETEDoituong') is not null drop proc DELETEDoituong;
+go
+
+create proc DELETEDoituong
+@id int
+as
+begin 
+	Delete DOITUONG where MaDoiTuong=@id;
+end
+go
+
+if OBJECT_ID('EDITDoituong') is not null drop proc EDITDoituong;
+go
+
+create proc EDITDoituong
+@MaDoiTuong int ,
+@LoaiDoiTuong varchar(100),
+@DiemCongDTuong float
+as
+begin 
+	declare @checkmadoituong int
+	select @checkmadoituong=MADOITUONG 
+	from DOITUONG
+	where @MaDoiTuong=MaDoiTuong;
+
+	if @checkmadoituong = @MaDoiTuong
+		update DOITUONG set LoaiDoiTuong=@LoaiDoiTuong, DiemCongDTuong=@DiemCongDTuong where MADOITUONG=@MaDoiTuong
+	else
+		update DOITUONG set MaDoiTuong=@checkmadoituong,LoaiDoiTuong=@LoaiDoiTuong, DiemCongDTuong=@DiemCongDTuong where MADOITUONG=@MaDoiTuong 
+end
+go
+
+---function ngành 
+create function Fn_nganh()
+returns table return
+	select MaNganh, TenNganh, ChiTieu, DiemChuan
+	from NGANH
+
+go 
+select * from Fn_nganh()
+
 -----------------------
 --test
 
 
+declare @sbd int,
+@tennganh varchar(50),
+@khoi varchar(50),
+@nguyenvong int
+exec DELETENguyenVong @sbd=1,@tennganh='Su Pham Tieng Anh',@khoi='A',@nguyenvong=2;
+
+---------------------------------Test Trigger------------------------------------------------------
 
 
-declare @sbd int
-exec DELETEHoSoThiSinh @sbd=3;
+
+
+
+create trigger TriggerInsertNguyenVong
+on NGUYENVONG
+instead of Insert
+as
+declare @sbd int,@manganh varchar(50),@khoi varchar(10),@nguyenvong int,
+		@diemTB float,@danhgia bit,@TestRowCount int;
+select	@TestRowCount=COUNT(*) from inserted;
+if @TestRowCount=1
+	begin
+		select @sbd=SBD,@manganh=MaNganh,@khoi=Khoi,@nguyenvong=NguyenVong
+		from inserted;
+		declare @diemA float,@diemA1 float,@diemB float,@diemC float,@diemD float;
+		--if (@sbd is not null and @manganh is not null and @khoi is not null and @nguyenvong is not null)
+			--begin
+				--if @diemTB is null
+					--begin
+						select @diemA=Toan+Ly+Hoa
+						from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+						where Khoi=N'A' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+					--end;
+				--if @diemTB is null
+					--begin
+						select @diemA1=Toan+Ly+AnhVan
+						from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+						where Khoi=N'A1' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+					--end;
+				--if @diemTB is null
+					--begin
+						select @diemB=Sinh+Ly+Hoa
+						from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+						where Khoi=N'B' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+					--end;
+				--if @diemTB is null
+					--begin
+						select @diemC=Van+Su+Dia
+						from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+						where Khoi=N'C' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+					--end;
+				--if @diemTB is null
+					--begin
+						select @diemD=Toan+Ly+AnhVan
+						from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+						where Khoi=N'D1' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+					--end;
+				--if (@danhgia is null and @diemTB is not null)
+					--begin
+						select @danhgia=1
+						from NGUYENVONG join NGANH on NGUYENVONG.MaNganh=NGANH.MaNganh
+						where NGUYENVONG.SBD=@sbd and NGUYENVONG.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+					--end;
+				--if (@danhgia is null and @diemTB is not null)
+					--begin
+						select @danhgia=0
+						from NGUYENVONG join NGANH on NGUYENVONG.MaNganh=NGANH.MaNganh
+						where NGUYENVONG.SBD=@sbd and NGUYENVONG.MaNganh=@manganh and @diemTB<NGANH.DiemChuan;
+					--end;
+				Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+				values(@sbd,@manganh,@khoi,@nguyenvong,@diemTB,@danhgia)
+			--end;
+		--else
+			--throw 50027,'Limit Insert to a single row.',1;
+	end;
+go
+create trigger TriggerInsertNguyenVong
+on NGUYENVONG
+instead of Insert
+as
+declare @sbd int,@manganh varchar(50),@khoi varchar(10),@nguyenvong int,
+		@diemTB float,@danhgia varchar(50),@TestRowCount int;
+select	@TestRowCount=COUNT(*) from inserted;
+if @TestRowCount=1
+	begin
+		select @sbd=SBD,@manganh=MaNganh,@khoi=Khoi,@nguyenvong=NguyenVong
+		from inserted;
+
+		--declare @diemA float,@diemA1 float,@diemB float,@diemC float,@diemD float;
+		if (@khoi=N'A')
+			begin
+				print'vao dung khoi';
+				--declare @diemTB float
+				--declare @danhgia varchar(50)
+				select @diemTB=Toan+Ly+Hoa
+				from HOSOTHISINH
+				where SBD=1;
+				if(@diemTB>0)
+				begin
+					print'vao dung diem';
+					select @danhgia='True'
+					from NGANH
+					where NGANH.MaNganh=7140231 and @diemTB>NGANH.DiemChuan;
+
+					select TenNganh
+					from NGANH
+					where NGANH.MaNganh=7140231 and @diemTB>NGANH.DiemChuan;
+
+					Print '$' + convert(varchar,@danhgia,1);
+					select @danhgia;
+					print 'hello';
+					Insert NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong,DiemTB,DanhGia)
+					values(1,7140231,'A',2,@diemTB,@danhgia)
+				end;
+				else
+				throw 50027,'Limit Insert to a single row.',1;
+			end
+			
+		--if (@sbd is not null and @manganh is not null and @khoi is not null and @nguyenvong is not null)
+		--	--begin
+		--		--if @diemTB is null
+		--			--begin
+		--				select @diemA=Toan+Ly+Hoa
+		--				from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+		--				where Khoi=N'A' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+		--			--end;
+		--		--if @diemTB is null
+		--			--begin
+		--				select @diemA1=Toan+Ly+AnhVan
+		--				from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+		--				where Khoi=N'A1' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+		--			--end;
+		--		--if @diemTB is null
+		--			--begin
+		--				select @diemB=Sinh+Ly+Hoa
+		--				from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+		--				where Khoi=N'B' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+		--			--end;
+		--		--if @diemTB is null
+		--			--begin
+		--				select @diemC=Van+Su+Dia
+		--				from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+		--				where Khoi=N'C' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+		--			--end;
+		--		--if @diemTB is null
+		--			--begin
+		--				select @diemD=Toan+Ly+AnhVan
+		--				from NGUYENVONG join HOSOTHISINH on NGUYENVONG.SBD=HOSOTHISINH.SBD
+		--				where Khoi=N'D1' and NGUYENVONG.SBD=@sbd --and NGUYENVONG.MaNganh=@manganh;
+		--			--end;
+		--		--if (@danhgia is null and @diemTB is not null)
+		--			--begin
+		--				select @danhgia=1
+		--				from NGUYENVONG join NGANH on NGUYENVONG.MaNganh=NGANH.MaNganh
+		--				where NGUYENVONG.SBD=@sbd and NGUYENVONG.MaNganh=@manganh and @diemTB>NGANH.DiemChuan;
+		--			--end;
+		--		--if (@danhgia is null and @diemTB is not null)
+		--			--begin
+		--				select @danhgia=0
+		--				from NGUYENVONG join NGANH on NGUYENVONG.MaNganh=NGANH.MaNganh
+		--				where NGUYENVONG.SBD=@sbd and NGUYENVONG.MaNganh=@manganh and @diemTB<NGANH.DiemChuan;
+		--			--end;
+					
+		--	--end;
+		----else
+		--	--throw 50027,'Limit Insert to a single row.',1;
+	end
+
+go
+-----
+declare @sbd int,@tennganh varchar(50),@khoi varchar(50),@nguyenvong int
+
+INSERT NGUYENVONG (SBD,MaNganh,Khoi,NguyenVong)
+values (1,'7140231','A',1)
+
+select * from NGANH 
+
+select * from NGUYENVONG
+
+select * from HOSOTHISINH
+
+delete NGUYENVONG where SBD=1;
+
+
+
+insert NGANH(MaNganh, TenNganh, ChiTieu, DiemChuan) values
+('7140231',	'Su Pham Tieng Anh111',30,23.04)
+
+select TenNganh
+from NGUYENVONG join NGANH on NGUYENVONG.MaNganh=NGANH.MaNganh
+where SBD=1 and NGUYENVONG.MaNganh=7140231 --and 30>NGANH.DiemChuan;
+
 
 select *
-from HOSOTHISINH
-
-
-select HOKHAU.TenHuyen,HOKHAU.TenTinh,KHUVUC.DiemCongKV
-from HOKHAU join KHUVUC on HOKHAU.MaHoKhau=KHUVUC.MaKhuVuc
-where (KHUVUC.MaKhuVuc)=cast('II' as  int);
-
-select TenKhuVuc
-from KHUVUC
-where MaKhuVuc='II'
-
-select SBD, HoDem,Ten,NgaySinh,GioiTinh,TenHuyen,TenTinh, LoaiDoiTuong, TenDanToc
-from ((HOSOTHISINH join HOKHAU on HOSOTHISINH.MaHoKhau=HOKHAU.MaHoKhau) 
-		join DOITUONG on HOSOTHISINH.MaDoiTuong=DOITUONG.MaDoiTuong)
-		join DANTOC on HOSOTHISINH.MaDanToc=DANTOC.MaDanToc
-
-
-
-
-
-exec LOADTableHoSoThiSinh
+from NGANH
+where TenNganh like '@ten%'
--- SQLQuery1_vuong.sql
+++ SQLQuery1_vuong.sql
@@ -12,8 +12,8 @@ use QuanLyTuyenSinh;
 go
 
 if OBJECT_ID('Account') is not null drop table Account;
-go
-------------------------Acount----------------------
+go-------Acount--
+-------------------------------------
 create table Account
 (
 ID int Identity(1,1),
@@ -62,7 +62,7 @@ values
 go
 ---------------------Nganh------------------------- 
 create table NGANH(
-MaNganh varchar (50) not null,
+MaNganh int not null,
 TenNganh varchar(100) not null,
 ChiTieu int,
 DiemChuan float,
@@ -71,91 +71,60 @@ CONSTRAINT pk_NGANH primary key(MaNganh)
 go
 
 insert NGANH(MaNganh, TenNganh, ChiTieu, DiemChuan) values
-('7140231D',	'Su Pham Tieng Anh (Dai Tra)',30,23.04),	
-('7210403D',	'Thiet Ke Do Hoa (Dại Tra)',30,21),	
-('7210404D',	'Thiet Ke Thoi Trang (Dai Tra)',30,18.44),	
-('7220201D',	'Ngon Ngu Anh (Dai Tra)',30	,22.3),	
-('7340120D',	'Kinh Doanh Quoc Te (Dai Tra)',30	,	21.6),	
-('7340122D',	'Thuong Mai Dien Tu (Dai Tra)',30	,	22.4),	
-('7340301C',	'Ke Toan (Chat Luong Cao Tieng Viet)',30	,	19),	
-('7340301D',	'Ke Toan (Dai Tra)',30	,	21.1),	
-('7480108A',	'Con Nghe Ky Thuat May Tinh (Chat Luong Cao Tieng Anh)',30	,20.35),	
-('7480108C',	'Cong Nghe Ky Thuat May Tinh (Chat Luong Cao Tieng Viet)',30	,21.2),	
-('7480108D',	'Cong Nghe Ky Thuat May Tinh (Dai Tra)',30	,	22.9),	
-('7480201A',	'Cong Nghe Thong Tin (Chat Luong Cao Tieng Anh)',30	,	21.8),	
-('7480201C',	'Cong Nghe Thong Tin (Chat Luong Cao Tieng Viet)',30	,	22.3),	
-('7480201D',	'Cong Nghe Thong Tin (Dai Tra)',30	,	23.9),	
-('7480203D',	'Ky Thuat Du Lieu (Dai Tra)',30	,22.2),	
-('7510102A',	'Cong Nghe Ky Thuat Cong Trinh Xay Dung (Chat Luong Cao Tieng Anh)',30	, 17.8),	
-('7510102C',	'Cong Nghe Ky Thuat Cong Trinh Xay Dung (Chat Luong Cao Tieng Viet)',30	,18.6),	
-('7510102D',	'Cong Nghe Ky Thuat Cong Trinh Xay Dung (Dai Tra)',30	,	21.3),	
-('7510106D',	'He Thong Cong Trinh Xay Dung (Dai Tra)',30	,19.2),	
-('7510201A',	'Cong Nghe Ky Thuat Co Khi (Chat Luong Cao Tieng Anh)',30	,	20.5),	
-('7510201C',	'Cong Nghe Ky Thuat Co Khi (Chat Luong Cao Tieng Viet)',30	,21.4),	
-('7510201D',	'Cong Nghe Ky Thuat Co Khi (Dai Tra)',30	, 22.9),	
-('7510202A',	'CN Che Tao May (Chat Luong Cao Tieng Anh)',30	,19.5),	
-('7510202C',	'CN Che Tao May (Chat Luong Cao Tieng Viet)',30	,20.7),	
-('7510202D',	'CN Che Tao May (Dai Tra)',30	,21.9),	
-('7510203A',	'Cong Nghe Ky Thuat Co Dien Tu (Chat Luong Cao Tien Anh)',30	,	21),	
-('7510203C',	'Cong Nghe Ky Thuat Co Dien Tu (Chat Luong Cao Tien Viet)',30	,21.7),	
-('7510203D',	'Cong Nghe Ky Thuat Co Dien Tu (Dai Tra)',30	,23.1),	
-('7510205A',	'Cong Nghe Ky Thuat O To (Chat Luong Cao Tieng Anh)',30	,21.8),	
-('7510205C',	'Cong Nghe Ky Thuat O To (Chat Luong Cao Tieng Viet)',30	,22.7),	
-('7510205D',	'Cong Nghe Ky Thuat O To (Dai Tra)',30	,23.7),	
-('7510206A',	'Cong Nghe Ky Thuat Nhiet (Chat Luong Cao Tieng Anh)',30	,18.05),	
-('7510206C',	'Cong Nghe Ky Thuat Nhiet (Chat Luong Cao Tieng Viet)',30	,19.7),
-('7510206D',	'Cong Nghe Ky Thuat Nhiet (Dai Tra)',30	,21.5),	
-('7510208D',	'Nang Luong Tai Tao (Dai Tra)',30	,	21),	
-('7510209D',	'Robot va Tri Tue Nhan Tao (Dai Tra)',30	,25.2),	
-('7510301A',	'Cong Nghe Ky Thuat Dien, Dien Tu (Chat Luong Cao Tieng Anh)',30	,	19.3),	
-('7510301C',	'Cong Nghe Ky Thuat Dien, Dien Tu (Chat Luong Cao Tieng Viet)',30	,	21),	
-('7510301D',	'Cong Nghe Ky Thuat Dien, Dien Tu (Dai Tra)',30	, 22.8),	
-('7510302A',	'Cong Nghe Ky Thuat Dien Tu - Vien Thong (Chat Luong Cao Tieng Anh)',30	,18.4),	
-('7510302C',	'Cong Nghe Ky Thuat Dien Tu - Vien Thong (Chat Luong Cao Tieng Viet)',30	,	19.5),	
-('7510302D',	'Cong Nghe Ky Thuat Dien Tu - Vien Thong (Dai Tra)',30	,21.7),	
-('7510303A',	'Cong Nghe Ky Thuat Dieu Khien va Tu Dong Hoa (Chat Luong Cao Tieng Anh)',30	,	21.5),	
-('7510303C',	'Cong Nghe Ky Thuat Dieu Khien va Tu Dong Hoa (Chat Luong Cao Tieng Viet)',30	,	22.2),	
-('7510303D',	'Cong Nghe Ky Thuat Dieu Khien va Tu Dong Hoa (Dai Tra)',30	,  23.5),	
-('7510401D',	'Cong Nghe Ky Thuat Hoa Hoc (Dai Tra)',30	, 22.4),
-('7510402D',	'Cong Nghe Vat Lieu (Dai Tra)',30	,18.55),	
-('7510406C',	'Cong Nghe Ky Thuat Moi Truong (Chat Luong Cao Tieng Viet)',30	,17),	
-('7510406D',	'Cong Nghe Ky Thuat Moi Truong (Dai Tra)',30	,19),	
-('7510601A',	'Quan Ly Cong Nghiep (Chat Luong Cao Tieng Anh)',30	,18.5),	
-('7510601C',	'Quan Ly Cong Nghiep (Chat Luong Cao Tieng Viet)',30	,20.2),	
-('7510601D',	'Quan Ly Cong Nghiep (Dai Tra)',30	,22.2),	
-('7510605D',	'Logistics & Quan Ly Chuoi Cung Ung (Dai Tra)',30	,23.3),	
-('7510801C',	'Cong Nghe Ky Thuat In (Chat Luong Cao Tieng Viet)',30	,18.2),	
-('7510801D',	'Cong Nghe Ky Thuat In (Dai Tra)',30	, 20.3),	
-('7520117D',	'KT Cong Nghiep (Dai Tra)',30	,19.8),	
-('7520212D',	'Ky Thuat Y Sinh (Dien Tu YS) (Dai Tra)',30	,20.75),	
-('7540101A',	'Cong Nghe Thuc Pham (Chat Luong Cao Tieng Anh)',30	, 18.45),	
-('7540101C',	'Cong Nghe Thuc Pham (Chat Luong Cao Tieng Viet)',30	, 20),	
-('7540101D',	'Cong Nghe Thuc Pham (Dai Tra)',30	, 22.2),	
-('7540203D',	'Cong Nghe Vat Lieu Det May (Dai Tra)',30	, 18),	
-('7540204C',	'Cong Nghe May (Chat Luong Cao Tieng Viet)',30	,18.2),	
-('7540204D',	'Cong Nghe May (Dai Tra)',30	, 21.1),	
-('7549002D',	'Ky Thuat Go va Noi That (Dai Tra)',30	, 18.5),	
-('7580101D',	'Kien Truc (Dai Tra)',30	, 20.33),	
-('7580205D',	'KT Xay Dung Cong Trinh Giao Thong (Dai Tra)',30	,18.4),	
-('7580302D',	'Quan Ly Xay Dung (Dai Tra)',30	,20.3),	
-('7810202D',	'Quan Tri Nha Hang va Dich Vu An Uong (Dai Tra)',30	, 21.4),	
-('7810502D',	'Ky Thuat Nu Cong (Dai Tra)',30	,18)
+('7140231',	'Su Pham Tieng Anh',30,23.04),	
+('7210403',	'Thiet Ke Do Hoa',30,21),	
+('7210404',	'Thiet Ke Thoi Trang',30,18.44),	
+('7220201',	'Ngon Ngu Anh',30	,22.3),	
+('7340120',	'Kinh Doanh Quoc Te',30	,	21.6),	
+('7340122',	'Thuong Mai Dien Tu',30	,	22.4),	
+('7340301',	'Ke Toan',30	,	19),	
+('7480108',	'Con Nghe Ky Thuat May Tinh',30	,20.35),	
+('7480201',	'Cong Nghe Thong Tin',30	,	21.8),		
+('7480203',	'Ky Thuat Du Lieu',30	,22.2),	
+('7510102',	'Cong Nghe Ky Thuat Cong Trinh Xay Dung',30	, 17.8),	
+('7510106',	'He Thong Cong Trinh Xay Dung',30	,19.2),	
+('7510201',	'Cong Nghe Ky Thuat Co Khi',30	,	20.5),	
+('7510202',	'CN Che Tao May',30	,19.5),	
+('7510203',	'Cong Nghe Ky Thuat Co Dien Tu',30	,	21),	
+('7510205',	'Cong Nghe Ky Thuat O To',30	,21.8),	
+('7510206',	'Cong Nghe Ky Thuat Nhiet',30	,18.05),	
+('7510208',	'Nang Luong Tai Tao',30	,	21),	
+('7510301',	'Cong Nghe Ky Thuat Dien, Dien Tu',30	,	19.3),	
+('7510302',	'Cong Nghe Ky Thuat Dien Tu - Vien Thong',30	,18.4),	
+('7510303',	'Cong Nghe Ky Thuat Dieu Khien va Tu Dong Hoa',30	,	21.5),	
+('7510401',	'Cong Nghe Ky Thuat Hoa Hoc',30	, 22.4),
+('7510402',	'Cong Nghe Vat Lieu',30	,18.55),	
+('7510406',	'Cong Nghe Ky Thuat Moi Truong',30	,17),		
+('7510601',	'Quan Ly Cong Nghiep',30	,18.5),	
+('7510605',	'Logistics & Quan Ly Chuoi Cung Ung',30	,23.3),	
+('7510801',	'Cong Nghe Ky Thuat In',30	,18.2),	
+('7520117',	'KT Cong Nghiep',30	,19.8),	
+('7520212',	'Ky Thuat Y Sinh (Dien Tu YS)',30	,20.75),	
+('7540101',	'Cong Nghe Thuc Pham (Chat Luong Cao Tieng Anh)',30	, 18.45),		
+('7540203',	'Cong Nghe Vat Lieu Det May (Dai Tra)',30	, 18),	
+('7540204',	'Cong Nghe May (Dai Tra)',30	, 21.1),	
+('7549002',	'Ky Thuat Go va Noi That (Dai Tra)',30	, 18.5),	
+('7580101',	'Kien Truc (Dai Tra)',30	, 20.33),	
+('7580205',	'KT Xay Dung Cong Trinh Giao Thong (Dai Tra)',30	,18.4),	
+('7580302',	'Quan Ly Xay Dung (Dai Tra)',30	,20.3),	
+('7810202',	'Quan Tri Nha Hang va Dich Vu An Uong (Dai Tra)',30	, 21.4),	
+('7810502',	'Ky Thuat Nu Cong (Dai Tra)',30	,18)
 go
 --------------------Nguyen Vong---------------------
 create table NGUYENVONG(
 SBD int not null,
-MaNganh varchar(50) not null,
+MaNganh int not null,
 Khoi varchar(10) not null,
 NguyenVong int,
 DiemTB float,
-DanhGia bit,
+DanhGia varchar(50),
 constraint pk_NGUYENVONG primary key(SBD,MaNganh,Khoi)
 );
 Insert NGUYENVONG(SBD,MaNganh,Khoi,NguyenVong)
 values 
-(1,'7480201C','A',1),
-(2,'7480201C','A',1),
-(3,'7480201C','A',1)
+(1,'7480201','A',1),
+(2,'7480201','A',1),
+(3,'7480201','A',1)
 go
 --------------------Dan Toc-------------------
 create table DANTOC(
